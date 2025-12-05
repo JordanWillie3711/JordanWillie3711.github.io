ca428c9bb84a17047e79a85edc65f7599262c107
JordanWillie3711 <jordaniscool3711@gmail.com>
2025-12-05 17:16:42 +0000

    newPlatformer

--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -98,4 +98,6 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  sunflower: { image: "images/collectables/sun.png"},
+  darren: { image: 'images/collectables/DS.png'},
 };
